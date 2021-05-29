0a4310354dc3c3ba16105c64948d19e450ed7c79
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-05-29 00:58:00 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,14 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 from flask_bootstrap import Bootstrap
-
+from flask_mail import Mail
+from flask_login import LoginManager
 
 app = Flask(__name__, template_folder ='../templates', static_folder='../static')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-#login = LoginManager(app)
-#login.login_view = 'login'
+login = LoginManager(app)
+login.login_view = 'login'
 bootstrap = Bootstrap(app)
+mail = Mail(app)
+
 
 from app import routes, models
