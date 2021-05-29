aee9193b7d8cafb44fafc716e7ad704f88e926cf
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-05-29 03:55:55 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ import os
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_login import LoginManager
+from flask_principal import Permission, RoleNeed
 
 app = Flask(__name__, template_folder ='../templates', static_folder='../static')
 app.config.from_object(Config)
@@ -15,6 +16,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 mail = Mail(app)
-
+principals = Permission(app)
+enter_permission = Permission(RoleNeed('enter'))
 
 from app import routes, models
